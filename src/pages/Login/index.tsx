482356ca14c6f3f498966c92a0ba6c80ecccd1d5
liuzijian <253358001@qq.com>
2025-03-18 14:16:33 +0800

    feat:登录页面更新

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,9 @@
+import React from 'react';
+import { Button, Form, Input, Spin, Row, Col } from 'antd';
+import { history, useModel } from '@umijs/max';
 import { useRequest as CommonuseRequest, useLocale } from '@/hooks';
 import Auth from '@/services/auth';
 import type { IAuthLoginVo } from '@/services/auth/type';
-import { Button, Form, Input, Spin } from 'antd';
-import React from 'react';
-import { history } from 'umi';
-// import { t } from '@/i18n-lang'
-import { useModel } from '@umijs/max';
 
 const Login: React.FC = () => {
   const [form] = Form.useForm();
@@ -16,7 +14,6 @@ const Login: React.FC = () => {
 
   const loginRequest = CommonuseRequest(Auth.login, {
     onSuccess: (data: IAuthLoginVo) => {
-      console.log(data);
       history.push('/dashboard');
     },
   });
@@ -30,41 +27,59 @@ const Login: React.FC = () => {
   return (
     <div>
       <Spin spinning={loginRequest?.loading}>
-        <Form form={form} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }}>
-          <Form.Item
-            name="mobile"
-            label={t('auth_login_mobile')}
-            rules={[
-              {
-                required: true,
-                message: t('auth_login_rule_mobile_message'),
-              },
-            ]}
+        <Row
+          align='middle'
+          style={{
+            width: '100%',
+            height: '100vh'
+          }}
+        >
+          <Col
+            span={24}
           >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label={t('auth_login_password')}
-            rules={[
-              {
-                required: true,
-                message: t('auth_login_rule_password_message'),
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
-            <Button
-              type="primary"
-              onClick={submit}
-              disabled={loginRequest?.loading}
+            <Form
+              form={form}
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 16 }}
             >
-              {t('auth_login_confirm')}
-            </Button>
-          </Form.Item>
-        </Form>
+              <Form.Item
+                name="mobile"
+                label={t('auth_login_mobile')}
+                rules={[
+                  {
+                    required: true,
+                    message: t('auth_login_rule_mobile_message'),
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                label={t('auth_login_password')}
+                rules={[
+                  {
+                    required: true,
+                    message: t('auth_login_rule_password_message'),
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
+                <Button
+                  type="primary"
+                  onClick={submit}
+                  disabled={loginRequest?.loading}
+                >
+                  {t('auth_login_confirm')}
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+          
+        </Row>
+        
       </Spin>
     </div>
   );
